b0d8605cf1ff8521d3068278245dabc506c97472
Huang Shuhang <yhnbgfd@gmail.com>
2014-02-08 22:10:12 +0800

    asd

--- WpfApplication1/Database/DML.cs
+++ WpfApplication1/Database/DML.cs
@@ -10,10 +10,18 @@ namespace Wpf.Database
     public class DML
     {
         SQLiteCommand cmd = new SQLiteCommand();
+        DataSet data = new DataSet();
+        SQLiteDataAdapter SDataAdapter;
+
+        string SelectSQL;
+        string InsertSQL;
 
         public DML()
         {
             cmd.Connection = new DBconnect().Connect();
+
+            InsertSQL = "insert into T_Report(id,datetime,unitsname,use,income,expenses) values(:id,:datetime,:unitsname,:use,:income,:expenses);";
+
         }
 
         /// <summary>
@@ -21,9 +29,19 @@ namespace Wpf.Database
         /// </summary>
         public void Insert(string sql)
         {
-            cmd.CommandText = sql;
-            cmd.ExecuteNonQuery();
+            //cmd.CommandText = sql;
+            //cmd.ExecuteNonQuery();
+            object[] rowVals = new object[6];
+            rowVals[0] = 99;
+            rowVals[1] = "1989-03-17 11:11:11";
+            rowVals[2] = "nameTest";
+            rowVals[3] = "useTest";
+            rowVals[4] = 123.5;
+            rowVals[5] = 32.1;
 
+            data.Tables[0].Rows.Add(rowVals);
+            SDataAdapter.InsertCommand = new SQLiteCommand(InsertSQL, cmd.Connection);
+            SDataAdapter.Update(data.Tables[0]);
         }
 
         /// <summary>
@@ -52,7 +70,11 @@ namespace Wpf.Database
             DataSet dataset = new DataSet();
             cmd.CommandText = sql;
             SQLiteDataAdapter da = new SQLiteDataAdapter(cmd);
+            SDataAdapter = da;
             da.Fill(dataset);
+            data = dataset;
+
+            Insert(sql);
             return dataset;
         }
 
