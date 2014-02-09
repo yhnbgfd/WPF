c647c113a4867004bfd1149bbdfe26c858203bcf
Huang Shuhang <yhnbgfd@gmail.com>
2014-02-10 02:27:20 +0800

    添加excel包，这是2013版本的，不知道兼容性如何。

--- WpfApplication3/MainWindow.xaml.cs
+++ WpfApplication3/MainWindow.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using Excel = Microsoft.Office.Interop.Excel;
 using Wpf.Data;
 
 namespace Wpf
