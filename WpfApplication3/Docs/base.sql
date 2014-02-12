f6d51a1bfd1855bca5e105b6dc495143cf68c698
Huang Shuhang <yhnbgfd@gmail.com>
2014-02-12 18:06:00 +0800

    更新sql.sql

--- WpfApplication3/Docs/base.sql
+++ WpfApplication3/Docs/base.sql
@@ -1,31 +1,38 @@
-create table T_Report(
-	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-	datetime timestamp,
-	unitsname text,
-	use text,
-	income real,
-	expenses real,
-	type integer
+CREATE TABLE "main"."T_Report" (
+"ID"  INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+"DateTime"  TIMESTAMP,
+"UnitsName"  TEXT DEFAULT NULL,
+"Use"  TEXT DEFAULT NULL,
+"Income"  REAL DEFAULT 0,
+"Expenses"  REAL DEFAULT 0,
+"Type"  INTEGER
 );
 
-create table T_Type(
-	id integer,
-	name text
+CREATE TABLE "main"."T_User" (
+"id"  INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+"username"  TEXT NOT NULL,
+"password"  TEXT,
+"Status"  INTEGER DEFAULT 0,
+"remark"  TEXT
 );
+insert into main.T_User(username,password) values('admin','123');
 
 CREATE TABLE "main"."T_Log" (
-	"ID"  INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-	"TIME"  timestamp,
-	"TITLE"  TEXT,
-	"CONTENT"  TEXT,
-	"REMARK"  TEXT,
-	"TYPE"  TEXT
-);
+"ID"  INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+"TIME"  timestamp,
+"TITLE"  TEXT,
+"CONTENT"  TEXT,
+"REMARK"  TEXT,
+"TYPE"  TEXT
+)
+;
+
+
 
 CREATE TABLE "main"."T_Surplus" (
 "id"  INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 "year"  INTEGER,
 "month"  INTEGER,
-"surplus"  REAL,
-type integer
+"surplus"  REAL DEFAULT 0,
+"type"  INTEGER
 );
