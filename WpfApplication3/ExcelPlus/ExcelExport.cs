792be113c62a15192344ff2fc21be029dcc2e0a8
henryWhong <hernthay@hotmail.com>
2014-02-13 21:47:08 +0800

    导出优化

--- WpfApplication3/ExcelPlus/ExcelExport.cs
+++ WpfApplication3/ExcelPlus/ExcelExport.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Reflection; 
+using System.Reflection;
 using Microsoft.Office.Interop.Excel;
 using System.Data;
 
@@ -12,205 +12,155 @@ namespace Wpf.ExcelPlus
     {
         //class variable
         private List<Object[]> contentArray = new List<Object[]>();
-        private int exportYear = 2014;
-        private int exportMonth = 1;
-        private int exportType = 1;
         private int surplusCount = 1;
         private double countIncome = 0;
         private double countExpenses = 0;
 
-        //Com variable
-        private Application xlApp;
-        private Workbook wb;
-
-        public ExcelExport()
-        {
-            //测试用
-            Export(exportYear, exportMonth, exportType);
-            //正式版用
-           // ExportExcel(exportYear, exportMonth, exportType);
-        }
-
-        /*
-        public void ExportExcel(int year, int month, int type)
-        {
-            Init();
-
-            xlApp = new Application();
-            xlApp.Visible = true;
-
-            wb = xlApp.Workbooks.Open(AppDomain.CurrentDomain.BaseDirectory + "templt.xls");
-            Worksheet ws = (Worksheet)wb.Worksheets[1];
-            GetAndProcessData();
-
-        }
-
-        private void Init()
-        {
-            exportYear = 0;
-            exportMonth = 0;
-            exportType = 0;
-        }
-
-        private void UnInit()
-        {
- 
-        }
-
-        private void GetAndProcessData()
-        {
-            StringBuilder sql = new StringBuilder();
-            sql.Append("SELECT * FROM T_Report where type =" + exportType);
-            DataSet data = new Wpf.Data.Database().Select(sql.ToString());
-
-            int surplusYear = exportYear;
-            int surplusMont = exportMonth - 1;
-            if (exportMonth == 1)
-            {
-                surplusMont = 12;
-                surplusYear = surplusYear - 1;
-            }
-
-            string surplusql = "SELECT * FROM surplus where year =" + surplusYear + " and month=" + surplusMont + " and type=" + exportType;
-            DataSet surplusData = new Wpf.Data.Database().Select(surplusql);
-            foreach (DataRow dr in data.Tables[0].Rows)
-            {
-
-            }
-            
-            foreach (DataRow dr in data.Tables[0].Rows)
-            {
-                if (dr[1].ToString() != "")
-                {
-                    DateTime drTime = (DateTime)dr[1];
-
-                    if (drTime.Year == exportYear)
-                    {
-                        if (drTime.Month == exportMonth)
-                        {
-                            Object[] args = new Object[7];
-                            args[0] = drTime.Month;
-                            args[1] = drTime.Day;
-                            args[2] = dr[2].ToString();
-                            args[3] = dr[3].ToString();
-                            args[4] = (double)dr[4];
-                            args[5] = (double)dr[5];
-                            args[6] = (double)dr[4] - (double)dr[5];
-                            contentArray.Add(args);
-                        }
-                    }
-                }
-            }
-        }
-
-         */
-
         public void Export(int year, int month, int type)
         {
-            StringBuilder sql = new StringBuilder();
-            sql.Append("SELECT * FROM T_Report where type =" + exportType);
-            DataSet data = Wpf.Data.Database.Select(sql.ToString());
-
-            int surplusYear = exportYear;
-            int surplusMont = exportMonth - 1;
-            if (exportMonth == 1)
-            {
-                surplusMont = 12;
-                surplusYear = surplusYear - 1;
-            }
-
-            string surplusql = "SELECT surplus FROM T_Surplus where year =" + surplusYear + " and month=" + surplusMont + " and type=" + exportType;
-            double surplus = Wpf.Data.Database.SelectSurplus(surplusql);
+            string dataSql = ProcessDataSql(year, month, type);
+            DataSet data = Wpf.Data.Database.Select(dataSql);
+            string surplusSql = ProcessSurpluSql(year, month, type);
+            double surplus = Wpf.Data.Database.SelectSurplus(surplusSql);
 
             foreach (DataRow dr in data.Tables[0].Rows)
             {
                 if (dr[1].ToString() != "")
                 {
                     DateTime drTime = (DateTime)dr[1];
-                    if (drTime.Year == year)
+                    if (surplusCount % 25 == 1)
                     {
-                        if (drTime.Month == month)
-                        {
-                            if (surplusCount % 26 == 1)
-                            {
-                                Object[] conclusionCell = new Object[7];
-                                if (surplusCount == 1)
-                                {
-                                    conclusionCell[2] = "承上年总结";
-                                }
-                                else
-                                {
-                                    conclusionCell[2] = "承上页";
-                                }
-                                conclusionCell[0] = drTime.Month;
-                                conclusionCell[1] = drTime.Day;
-                                conclusionCell[3] = "";
-                                conclusionCell[4] = "";
-                                conclusionCell[5] = "";
-                                conclusionCell[6] = surplus;
-                                contentArray.Add(conclusionCell);
-                            }
-                            Object[] args = new Object[7];
-                            args[0] = drTime.Month;
-                            args[1] = drTime.Day;
-                            args[2] = dr[2].ToString();
-                            args[3] = dr[3].ToString();
-                            args[4] = (double)dr[4];
-                            args[5] = (double)dr[5];
-                            args[6] = surplus;
-                            contentArray.Add(args);
-
-                            surplus = (double)dr[4] - (double)dr[5] + surplus;
-                            countIncome += (double)dr[4];
-                            countExpenses += (double)dr[5];
-                            surplusCount++;
-                        }
+                        Object[] conclusionCell = new Object[7];
+                        if (surplusCount == 1)
+                            conclusionCell[2] = "承上年总结";
+                        else
+                            conclusionCell[2] = "承上页";
+                        conclusionCell[0] = drTime.Month;
+                        conclusionCell[1] = drTime.Day;
+                        conclusionCell[3] = "";
+                        conclusionCell[4] = "";
+                        conclusionCell[5] = "";
+                        conclusionCell[6] = surplus;
+                        contentArray.Add(conclusionCell);
                     }
+                    Object[] args = new Object[7];
+                    args[0] = drTime.Month;
+                    args[1] = drTime.Day;
+                    args[2] = dr[2].ToString();
+                    args[3] = dr[3].ToString();
+                    args[4] = (double)dr[4];
+                    args[5] = (double)dr[5];
+                    surplus = (double)dr[4] - (double)dr[5] + surplus;
+                    args[6] = surplus;
+                    contentArray.Add(args);
+                    countIncome += (double)dr[4];
+                    countExpenses += (double)dr[5];
+                    surplusCount++;
                 }
             }
 
             int countT = contentArray.Count + 3;
             Application xlApp = new Application();
-            xlApp.Visible = true;
 
-            Workbook wb = xlApp.Workbooks.Open(AppDomain.CurrentDomain.BaseDirectory + "templt.xls");
+            Workbook wb = null;
+            try
+            {
+                wb = xlApp.Workbooks.Open(AppDomain.CurrentDomain.BaseDirectory + "templt.xls");
+            }
+            catch (Exception)
+            {
+                System.Windows.MessageBox.Show("找不到导出模板，请重新设置模板", "出错了");
+                xlApp.Quit();
+                return;
+            }
+
+            xlApp.Visible = true;
             Worksheet ws = (Worksheet)wb.Worksheets[1];
             ws.Cells[2, 1] = String.Format("{0}年", year);
-            
+
             for (int i = 3; i < countT; i++)
             {
                 for (int j = 0; j < 7; j++)
                 {
                     string textAbc = contentArray[i - 3][j].ToString();
-                    ws.Cells[i + 1, j + 1] = textAbc;
+                    try
+                    {
+                        ws.Cells[i + 1, j + 1] = textAbc;
+                    }
+                    catch (Exception ee)
+                    {
+                        Console.WriteLine("出错内容" + textAbc+ "============="+ee.ToString());
+                    }
                 }
             }
 
-            string endCellCall = "G" + countT;
+            string endCellCall = "G" + (countT + 1);
             Range textRange = ws.get_Range("A3", endCellCall);
-            //textRange.HorizontalAlignment = XlHAlign.xlHAlignCenter;
+            textRange.HorizontalAlignment = XlHAlign.xlHAlignCenter;
             textRange.Borders.LineStyle = Microsoft.Office.Interop.Excel.XlLineStyle.xlContinuous;
+
+            ws.Cells[countT + 1, 3] = "借方发生额累计";
+            ws.Cells[countT + 1, 4] = countIncome.ToString();
+            ws.Cells[countT + 1, 5] = "贷方发生额累计";
+            ws.Cells[countT + 1, 6] = countExpenses.ToString();
 
             switch (type)
             {
                 case 1:
-                    ws.Cells[1, 5] = String.Format("(预算内户)", year);
+                    ws.Cells[1, 5] = "(预算内户)";
                     break;
                 case 2:
-                    ws.Cells[1, 5] = String.Format("(预算外户)", year);
+                    ws.Cells[1, 5] = "(预算外户)";
                     break;
                 case 3:
-                    ws.Cells[1, 5] = String.Format("(周转金户)", year);
+                    ws.Cells[1, 5] = "(周转金户)";
                     break;
                 case 4:
-                    ws.Cells[1, 5] = String.Format("(计生专户)", year);
+                    ws.Cells[1, 5] = "(计生专户)";
                     break;
                 case 5:
-                    ws.Cells[1, 5] = String.Format("(政粮补贴资金专户)", year);
+                    ws.Cells[1, 5] = "(政粮补贴资金专户)";
                     break;
                 default:
                     break;
             }
+        }
+
+        private string ProcessDataSql(int year, int month, int type)
+        {
+            string sql = "";
+            string curMonth = "";
+            string nextMonth = "";
+            string nextYear = "";
+            if (month == 12)
+                nextYear = (year + 1).ToString();
+            else
+                nextYear = year.ToString();
+            if (month < 10)
+                curMonth = "0" + month;
+            else
+                curMonth = month.ToString();
+            if (month < 9)
+                nextMonth = "0" + (month + 1).ToString();
+            else
+                nextMonth = (month + 1).ToString();
+            sql = "SELECT * FROM T_Report WHERE DateTime < '" + nextYear + "-" + nextMonth + "-01' AND DateTime >= '"
+                + year.ToString() + "-" + curMonth + "-01' AND Type = " + type + " ORDER BY DateTime;";
+            return sql;
+        }
+
+        private string ProcessSurpluSql(int year, int month, int type)
+        {
+            int surplusYear = year;
+            int surplusMont = month - 1;
+            if (month == 1)
+            {
+                surplusMont = 12;
+                surplusYear = surplusYear - 1;
+            }
+            string sql = "SELECT surplus FROM T_Surplus where year =" + surplusYear + " and month="
+                + surplusMont + " and type=" + type + ";";
+            return sql;
         }
     }
 }
