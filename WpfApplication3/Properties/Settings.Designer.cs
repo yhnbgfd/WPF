b4ead66e232c8d8369cbf4e980a29372eede140f
Huang Shuhang <yhnbgfd@gmail.com>
2014-02-18 02:01:16 +0800

    asd

--- WpfApplication3/Properties/Settings.Designer.cs
+++ WpfApplication3/Properties/Settings.Designer.cs
@@ -205,7 +205,7 @@ namespace Wpf.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool 正式版 {
             get {
                 return ((bool)(this["正式版"]));
