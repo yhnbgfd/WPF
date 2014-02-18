d3bda62d0706c89cf1a1e6f6fdb14aa6edb07292
Huang Shuhang <yhnbgfd@gmail.com>
2014-02-18 21:15:20 +0800

    增加程序单例

--- WpfApplication3/App.xaml.cs
+++ WpfApplication3/App.xaml.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Windows;
+using System.Diagnostics;
 
 namespace Wpf
 {
@@ -12,5 +13,20 @@ namespace Wpf
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Get Reference to the current Process
+            Process thisProc = Process.GetCurrentProcess();
+            // Check how many total processes have the same name as the current one
+            if (Process.GetProcessesByName(thisProc.ProcessName).Length > 1)
+            {
+                // If ther is more than one, than it is already running.
+                MessageBox.Show("金字塔财务管理工具正在运行。");
+                Application.Current.Shutdown();
+                return;
+            }
+
+            base.OnStartup(e);
+        }
     }
 }
