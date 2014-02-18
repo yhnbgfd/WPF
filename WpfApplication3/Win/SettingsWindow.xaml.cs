aa42749e419814aa52711ff0a62d851833a2a0a0
lijianbin <361931469@qq.com>
2014-02-18 22:17:20 +0800

    新增一个按钮

--- WpfApplication3/Win/SettingsWindow.xaml.cs
+++ WpfApplication3/Win/SettingsWindow.xaml.cs
@@ -100,5 +100,10 @@ namespace Wpf.Win
             Wpf.Helper.Secure.Save初始金额();
             this.TextBlock_保存成功.Text = "保存成功。";
         }
+
+        private void Button_Sure_Click(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Hidden;
+        }
     }
 }
