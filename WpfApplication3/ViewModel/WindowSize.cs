00ba37a9fdd19ac28381ca279c5123665ddad1d8
Huang Shuhang <yhnbgfd@gmail.com>
2014-02-27 14:42:59 +0800

    阿斯顿

---
+++ WpfApplication3/ViewModel/WindowSize.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Wpf.ViewModel
+{
+    static class WindowSize
+    {
+        static Rect WorkArea = SystemParameters.WorkArea;
+
+
+
+        public static void Max()
+        {
+
+        }
+
+        public static void Normal()
+        {
+
+        }
+
+        public static void Min()
+        {
+
+        }
+    }
+}
