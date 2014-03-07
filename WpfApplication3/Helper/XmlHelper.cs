7c861d981260845c20d8a6c3d42a1cea75d11a69
Huang Shuhang <yhnbgfd@gmail.com>
2014-03-07 20:49:58 +0800

    xml

---
+++ WpfApplication3/Helper/XmlHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Wpf.Helper
+{
+    public class XmlHelper
+    {
+        XmlDocument xmldoc;
+
+        public XmlHelper()
+        {
+            xmldoc = new XmlDocument();
+        }
+
+        public void ReadXML()
+        {
+
+        }
+
+        public void WriteXML()
+        {
+
+        }
+    }
+}
